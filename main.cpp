e0f3f534340d1357bd9f8779fd4b8eadbd1af454
amir_hs <ali01h72@gmail.com>
2026-04-01 19:07:21 +0300

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "softprintf.h"
+#include <stdio.h>
+
+int main(void) {
+
+    int a = 52;
+
+    printf ("a=%% b=%p c=%d\n", &a, 1488);
+    SoftPrintfTrampoline("a=%% b=%p c=%f\n", &a, -1.488);
+    return 0;
+}
---
+++ softprintf.h
@@ -0,0 +1,3 @@
+#pragma once
+
+extern "C" void SoftPrintfTrampoline(const char *fmt, ...);
